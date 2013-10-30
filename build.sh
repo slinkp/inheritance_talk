3c19fa99296ab7d118d97ffcb7718a01bbc326ca
Paul Winkler <paul@bitly.com>
2013-10-30 16:55:21 -0400

    More complete build script

--- build.sh
+++ build.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-source bin/activate
+# Make sure we're in the directory where this script lives.
+cd "${0%/*}" 2> /dev/null
 
-cd samples
+shout() { echo "$0: $*" >&2; }  # Just echo an error and the program name
+barf() { shout "$*"; exit 111; }
+safe() { "$@" || barf "cannot $*"; }
+
+if [ ! -d "bin" ]; then
+	safe virtualenv . 
+fi	
+
+source ./bin/activate || barf "No virtualenv?"
+
+# Install stuff if needed
+TMP_REQ=`mktemp -t req.txt`
+safe ./bin/pip freeze > $TMP_REQ 
+cmp -s $TMP_REQ requirements.txt || safe ./bin/pip install -r requirements.txt
+rm -f $TMP_REQ
+
+# Build the graphics
+
+safe cd samples
 
 for f in *py; do
     pyreverse -A -k $f
     mv classes_no_Name.dot $f.dot
 done
+
+dot -? > /dev/null 2>&1 || barf "You don't appear to have the 'dot' command. Install graphviz.  Eg. on OSX with Homebrew, try 'brew install graphviz'."
 
 for f in *dot; do
     dot $f -Tsvg -O
@@ -17,4 +38,6 @@ cd -
 
 mv samples/*svg static/
 
-hovercraft slides.rst html/
+# Build the slides.
+safe hovercraft slides.rst html/
+echo Output is in $PWD/html/
